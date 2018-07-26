c5cb1b69411feb09b6462885a8f79c6055a7348b
Sami Mäläskä <epsilon@welho.com>
2018-07-27 02:05:04 +0300

    Teht 12

---
+++ src/components/Maa.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Maa = (props) => {
+    console.log(props.naytaKaikki)
+
+    if (props.naytaKaikki) {
+        return (
+            <div>
+            <div>{props.nimi} {props.natiiviNimi}</div>
+            <div>capital: {props.pkaupunki}</div>
+            <div>population: {props.vaesto}</div>
+            <img src={props.lippuUrl} alt={'Flag of '+ props.nimi} />
+            </div>
+        )
+    }
+
+    return (
+        <div>{props.nimi}</div>
+    )
+}
+
+export default Maa
